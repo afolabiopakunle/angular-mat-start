91090939ea23f0acce6c148e3c987f97bc208256
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-12-27 22:35:28 +0100

    paused

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of, timer} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ecommerce';
+  isLoading = false;
+  options!: string[];
+  onChange(event: any) {
+    console.log(event)
+  }
+
+  ngOnInit() {
+    of(['Afolabi', "Opakunle", "Oluseun"]).subscribe(
+    data => {
+      this.isLoading = true
+      setTimeout(() => {
+        this.options = data
+        this.isLoading = false;
+      }, 3000)
+
+    }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSliderModule} from "@angular/material/slider";
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from "@angular/material/button";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
@@ -14,6 +20,12 @@ import {MatSliderModule} from "@angular/material/slider";
     BrowserModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    MatCheckboxModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
